610282ffdf802ac8da21304b3e9a77f076c4001d
davidsonmss <davidsonmss@hotmail.com>
2022-06-02 18:58:08 -0300

    aos 45 do segundo tempo.

--- test/specs/testeApidemos.js
+++ test/specs/testeApidemos.js
@@ -1,10 +1,13 @@
 describe("Testando a apidemos.apk", () => {
+    afterEach(() => {
+        driver.reset();
+    });
     it("Verificando se o nome do aplicativo é API Demos", async () => {
         const tituloApp = await $("android.widget.TextView").getText();
         expect(tituloApp).toBe("API Demos");
-        await $("//android.widget.TextView[@content-desc='App']").click();
     });
     it("Verificando se contém (Ok e o cancel) no Alert/Dialogs", async () => {
+        await $("//android.widget.TextView[@content-desc='App']").click();
         await $("//android.widget.TextView[@content-desc='Alert Dialogs']").click();
         var tituloAlert = await $("android.widget.TextView").getText();
         expect(tituloAlert).toBe("App/Alert Dialogs");
@@ -16,14 +19,17 @@ describe("Testando a apidemos.apk", () => {
         await $("id=android:id/button2").click();
     });
     it("Verificando as mensagens do LIST DIALOG", async () => {
-        await $('//android.widget.Button[@content-desc="List dialog"]').click();
+        await $("//android.widget.TextView[@content-desc='App']").click();
+        await $("//android.widget.TextView[@content-desc='Alert Dialogs']").click();
+        await $("id=select_button").click();
         var visibilidadeHeader = await $("id=android:id/alertTitle").getText();
         expect(visibilidadeHeader).toBe("Header title");
         await $("id=android:id/text1").click();
         var mensagemOne = await $("id=android:id/message").getText();
         expect(mensagemOne).toBe("You selected: 0 , Command one");
     });
-    xit("Verificando o botão Hide e Show", async () => {
+    it("Verificando o botão Hide e Show", async () => {
+        await $("//android.widget.TextView[@content-desc='App']").click();
         await $("//android.widget.TextView[@content-desc='Fragment']").click();
         await $('//android.widget.TextView[@content-desc="Hide and Show"]').click();
         var botaoHide = await $('(//android.widget.Button[@content-desc="Hide"])[1]').isDisplayed();
